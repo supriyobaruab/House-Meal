98087ac631226dd6352ca2562bf5fca25157556c
Supriyo Barua <59415473+supriyobaruab@users.noreply.github.com>
2025-08-07 10:08:14 +0600

    curved the table

--- public/info.js
+++ public/info.js
@@ -5,7 +5,7 @@ async function information() {
   const data = await response.json();
   data.data.forEach((element) => {
     table.innerHTML += `
-      <tr class="border-b">
+      <tr class="border-b bg-gray-50">
         <td class="px-4 w-48 py-2 ">${element.name.toUpperCase()}</td>
         <td class="px-4 w-48 py-2">${element.date}</td>
         <td class="px-4 w-64 py-2">৳${element.result}</td>
