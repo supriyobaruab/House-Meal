209a2f697f71acd50f8046e95abe99131cc55340
Supriyo Barua <59415473+supriyobaruab@users.noreply.github.com>
2025-09-02 00:02:56 +0600

    Update : made responsive

--- public/info.js
+++ public/info.js
@@ -1,5 +1,6 @@
 import { url } from "./app.js";
 const table = document.getElementById("table");
+const mobile_log = document.getElementById("mobile_log");
 async function information() {
   const response = await fetch(`${url}/logs`);
   const data = await response.json();
@@ -13,6 +14,17 @@ async function information() {
           ${element.description}
         </td>
       </tr>`;
+    mobile_log.innerHTML += `
+     <div
+      class="flex flex-col justify-evenly p-6 w-74 h-64 border-1 border-gray-200 rounded-xl space-y-1"
+    >
+      <p class=" "><span class="font-bold">Name : </span>${element.name.toUpperCase()}</p>
+      <p class=""><span class="font-bold">Fund : </span>${element.result}</p>
+      <p class=""><span class="font-bold">Date : </span>${element.date}</p>
+      <p class="">
+        <span class="font-bold">Description :</span> ${element.description}
+      </p>
+    </div>`;
   });
 }
 information();
