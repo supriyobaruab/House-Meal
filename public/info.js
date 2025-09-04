99920a61819837ae97d83d89e35e9d6c756be8a8
Supriyo Barua <59415473+supriyobaruab@users.noreply.github.com>
2025-09-04 08:44:38 +0600

    feat : expand on hover effect

--- public/info.js
+++ public/info.js
@@ -16,7 +16,7 @@ async function information() {
       </tr>`;
     mobile_log.innerHTML += `
      <div
-  class="grid grid-cols-2 gap-x-4 gap-y-2 p-8 w-78  border border-gray-200 rounded-xl shadow-md"
+  class="group grid grid-cols-2 gap-x-4 gap-y-2 p-8 w-78 h-54  border border-gray-200 rounded-xl shadow-md transition-all hover:h-64 duration-300 "
 >
   <div class="font-bold text-gray-500">Name :</div>
   <div class = "font-bold text-blue-700">${capitalizeFirst(element.name)}</div>
@@ -28,7 +28,9 @@ async function information() {
   <div>${element.date}</div>
 
   <div class="font-bold text-gray-500">Description :</div>
-  <div class="truncate overflow-y-auto">${element.description}</div>
+  <div class="truncate group-transition-all group-hover:whitespace-normal group-hover:break-words duration-300">${
+    element.description
+  }</div>
 </div>`;
   });
 }
